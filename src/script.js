069e6f8c55277f36c153b1c588386f1d5089bb23
Jankie06 <jadonwright0603@gmail.com>
2024-03-08 11:24:39 -0500

    Fix literally everything lol

--- src/script.js
+++ src/script.js
@@ -1,24 +1,24 @@
-window.onload = () => {
-  "use strict";
+// window.onload = () => {
+//   "use strict";
 
-  if ("serviceWorker" in navigator && document.URL.split(":")[0] !== "file") {
-    navigator.serviceWorker.register("./sw.js");
-  }
-}
+//   if ("serviceWorker" in navigator && document.URL.split(":")[0] !== "file") {
+//     navigator.serviceWorker.register("./sw.js");
+//   }
+// }
 
-document.addEventListener('DOMContentLoaded', function() {
-      var imageContainer = document.getElementById('image-container');
-      var selectedPosition = document.getElementById('selected-position');
+document.addEventListener('DOMContentLoaded', function () {
+  var imageContainer = document.getElementById('image-container');
+  var selectedPosition = document.getElementById('selected-position');
 
-      imageContainer.addEventListener('click', function(event) {
-        var x = event.pageX - this.offsetLeft;
-        var y = event.pageY - this.offsetTop;
+  imageContainer.addEventListener('click', function (event) {
+    var x = event.pageX - this.offsetLeft;
+    var y = event.pageY - this.offsetTop;
 
-        selectedPosition.style.left = (x - 5) + 'px'; // Adjust for the size of the dot
-        selectedPosition.style.top = (y - 5) + 'px'; // Adjust for the size of the dot
-      });
+    selectedPosition.style.left = (x - 5) + 'px'; // Adjust for the size of the dot
+    selectedPosition.style.top = (y - 5) + 'px'; // Adjust for the size of the dot
+  });
 });
-    
+
 
 //Owen's scroll management
 
@@ -58,10 +58,10 @@ function updateScroll(scroll) {
 var alreadyGenerated = false;
 
 function generateQRCodeYay() {
-  if (alreadyGenerated==true) {
+  if (alreadyGenerated == true) {
 
   }
-  var text = "This is all of yo data, yahoo: allstate, you're in good hands." + Math.random();
+  var text = getFormData();
   document.getElementById("qrcontainer").innerHTML = "";
   new QRCode(document.getElementById("qrcontainer"), text);
   document.getElementById("generateQR").textContent = "Refresh QR Code";
@@ -85,4 +85,44 @@ function increment(targetname) {
   var value = Number(currentElem.textContent);
   value++;
   currentElem.textContent = value;
+}
+
+function getFormData() {
+  var initial = document.getElementById("scouter-initials").value;
+  var level = getRadioData("level");
+  var match = document.getElementById("metch-yes").value;
+  var location = getRadioData("color");
+  var team = document.getElementById("teams-num").value;
+  var start = document.getElementById("selected-position").style.left + "-" + document.getElementById("selected-position").style.top;
+  var leave = String(document.querySelector(".leave-checkbox").checked);
+  var ampAuto = document.getElementById("count-amp").innerText;
+  var speakerAuto = document.getElementById("count-speaker").innerText;
+  var ampTeleop = document.getElementById("count-amp-tele").innerText;
+  var speakerTeleop = document.getElementById("count-speaker-tele").innerText;
+  var amped = document.getElementById("count-amplification-tele").innerText;
+  var pickup = getRadioData("pickup");
+  var skill = getRadioData("skill");
+  var defense = getRadioData("defense");
+  var speed = getRadioData("speed");
+  var died = String(document.querySelector(".died").checked);
+  var tip = String(document.querySelector(".tip").checked)
+  var dropped = String(document.querySelector(".drop").checked)
+  var comments = document.getElementById("comments").value
+  let data = [initial, level, match, location, team, start, leave, ampAuto, speakerAuto, ampTeleop, speakerTeleop, amped, pickup, skill, defense, speed, died, tip, dropped, comments];
+  let strings = "";
+  for (var i = 0; i < data.length; i++) {
+    strings += String(data[i]) + "\t";
+  } 
+  alert(strings);
+    return strings;
+}
+
+function getRadioData(elementName) {
+  var radios = document.getElementsByName(elementName);
+
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      return radios[i].id;
+    }
+  }
 }
